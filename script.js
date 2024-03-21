0ac93e04f17fc0a32c9f03f4352a5bd2d1bda95b
Angela Santos <145032472+ASPOLAKA@users.noreply.github.com>
2024-03-21 18:49:32 -0300

    Imagem e fundo

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert('Olá!')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+   
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //Bom Dia!
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#6A5ACD'
+    } else if(hora >= 12 && hora < 18) {
+        //Boa Tarde!
+        img.src = 'fototarde.png'
+        document.body.style.background = '#556B2F'
+    } else {
+        //Boa Noite!
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#2F4F4F'
+    }
+}
